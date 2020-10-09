e9140369882bb5aeb76de515ff57d890591429e7
Felipe Amador <felipedavidamador@gmail.com>
2020-10-08 21:47:15 -0300

    feat: add the basic structure

---
+++ server.py
@@ -0,0 +1,49 @@
+"""
+Contains the server start and health configurations.
+
+To see the uvicorn documentation, visit:
+https://www.uvicorn.org/
+"""
+import uvicorn
+
+from django.conf import settings
+
+
+class ASGIServer:
+    """Define the ASGI uvicorn settings configurations."""
+    def __init__(self) -> None:
+        """
+        Initialize the Server configurations.
+        """
+        self.debug: str = settings.DEBUG
+        self.default_server_host: str = "127.0.0.1"
+        self.default_server_port: int = 8000
+        self.server_host: str = settings.SERVER_HOST
+        self.server_port: int = settings.SERVER_PORT
+        self.asgi_application: str = settings.ASGI_APPLICATION
+
+    def __validate_debug(self) -> None:
+        """
+        Check if the required variables in non debug mode were passed.
+
+        :raises KeyError: If some required parameter were not passed.
+        """
+        if not self.debug:
+            if self.server_host is None or self.server_port is None:
+                raise KeyError("To use debug False, you must to specify the HOST and PORT variables.")
+
+        else:
+            self.server_host = self.default_server_host
+            self.server_port = int(self.default_server_port)
+
+    def run(self) -> None:
+        """
+        Run the server with uvicorn.
+        """
+        self.__validate_debug()
+
+        uvicorn.run(self.asgi_application, host=self.server_host, port=self.server_port)
+
+
+if __name__ == '__main__':
+    ASGIServer().run()
