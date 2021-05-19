f3672d44d6eb7a072f77987f323ef3e481c0dc67
Felipe Amador <felipedavidamador@gmail.com>
2021-05-19 18:55:33 -0300

    feat: create JsonResponseMixin

--- views.py
+++ views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from typing import Any, Dict
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.generic import ListView
+
+
+class JsonResponseMixin:
+    """A mixin that can be used to return an json response."""
+    content_type = "application/json"
+    response_class = JsonResponse
+
+    async def render_to_response(self, context: Dict[str, Any]) -> JsonResponse:
+        return self.response_class(
+            data=context,
+            content_type=self.content_type
+        )
+
+
+class MultipleObjectJsonResponseMixin():
+    ...
+
+
+class ApiListView(ListView):
+    ...
+
